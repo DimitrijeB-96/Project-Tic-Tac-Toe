a251bed36b37c0ec42edb15c00fd4618bfd649e1
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-06-07 13:04:33 +0200

    Reorganized code, made new IIFE functions for Rende and who is the Winner which was separated from Menu function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const boardBox = document.querySelectorAll('.board-box');
+
 const Player = (name, marker) => {
   let _name = name;
   let _marker = marker;
@@ -163,8 +165,16 @@ const GameController = (function() {
     playerO = [];
   }
 
+  function restartGame() {
+    board.restartBoard();
+    setFirstPlayerMarker();
+    clearPlayers();
+    gameOver();
+  }
+
   return {
     startGame,
+    restartGame,
     getRound,
     nextRound,
     getFirstPlayer,
@@ -180,119 +190,15 @@ const GameController = (function() {
   }
 })();
 
-const ScreenController = (function() {
-  const board = Board;
-  const gameController = GameController;
-
-  const displayMenuDiv = document.querySelector('.pop-up-menu');
-  const backgroundDiv = document.createElement('div');
-  const startGameBtn = document.querySelector('.btn-start');
-  const firstPlayerName = document.getElementById('first-player');
-  const secondPlayerName = document.getElementById('second-player');
-  showMenu();
-
-  startGameBtn.addEventListener('click', startGame);
-
+const RenderController = (function() {
   const displayTextForPlayer = document.querySelector('.current-player');
   const displayCurrentPlayer = document.querySelector('.current');
 
-  const displayWinnerDiv = document.querySelector('.pop-up-winner');
+  const backgroundDiv = document.createElement('div');
 
-  const displayWinnerH2 = document.querySelector('.show-winner-h1');
-  const displayWinnerP = document.querySelector('.show-winner');
-
-  const boardBox = document.querySelectorAll('.board-box');
-  boardBox.forEach(box => box.addEventListener('click', play));
-
-  const restartGameBtn = document.querySelector('.restart');
-  restartGameBtn.addEventListener('click', restartGame);
-
-  const newGameBtn = document.querySelector('.new-game');
-  newGameBtn.addEventListener('click', showMenu);
-
-  function startGame() {
-    gameController.startGame(firstPlayerName.value, secondPlayerName.value);
-    hideMenu();
-    restartGame();
-    currentPlayerText(gameController.getFirstPlayer().getName());
-  }
-
-  function play(e) { 
-    if (e.target.textContent === '') {
-      e.target.textContent = gameController.getCurrentPlayerMarker();
-      board.setBoard(e.target.id, gameController.getCurrentPlayerMarker());
-      gameController.storePlayersMarkers();
-      console.log(gameController.getRound());
-
-      let check = gameController.checkWinner();
-      if (check !== undefined && gameController.getRound() <= 9) {
-        if (check === 'X') {
-          displayGameOver(gameController.getFirstPlayer());
-        } else if (check === 'O') {
-          displayGameOver(gameController.getSecondPlayer());
-        } else if (check === 'TIE') {
-          displayGameOverWhenTie();
-        }
-        gameController.gameOver();
-      } else {
-        gameController.nextRound();
-        gameController.nextPlayerMarker();
-        currentPlayerText(gameController.nextPlayerName());
-      }
-    }
-  }
-
-  const playAgainBtn = document.querySelector('.play-again');
-  playAgainBtn.addEventListener('click', playAgain);
-
-  const menuBtn = document.querySelector('.menu');
-  menuBtn.addEventListener('click', openMenu);
-
-  function openMenu() {
-    hideWinner();
-    showMenu();
-    restartGame();
-  }
-
-  function playAgain() {
-    hideWinner();
-    restartGame();
-  }
-
-  function displayGameOver(player) {
-    hideCurrentPlayer();
-    showWinner();
-
-    displayWinnerH2.textContent = `Winner is `;
-    displayWinnerP.textContent = `${player.getName()}`;
-  }
-
-  function displayGameOverWhenTie() {
-    hideCurrentPlayer();
-    showWinner();
-
-    displayWinnerH2.textContent = '';
-    displayWinnerP.textContent = `IT'S A TIE!`;
-  }
-
-  function showWinner() {
-    displayWinnerDiv.style.display = 'flex';
-    addBackground();
-  }
-
-  function hideWinner() {
-    displayWinnerDiv.style.display = 'none';
-    removeBackground();
-  }
-
-  function showMenu() {
-    displayMenuDiv.style.display = 'flex';
-    addBackground();
-  }
-
-  function hideMenu() {
-    displayMenuDiv.style.display = 'none';
-    removeBackground();
+  function currentPlayerText(currentPlayer) {
+    displayTextForPlayer.style.display = 'inline';
+    displayCurrentPlayer.textContent = currentPlayer;
   }
 
   function addBackground() {
@@ -308,23 +214,205 @@ const ScreenController = (function() {
     displayTextForPlayer.style.display = 'none';
   }
 
-  function restartGame() {
-    board.restartBoard();
-    gameController.setFirstPlayerMarker();
-    currentPlayerText(gameController.getFirstPlayer().getName());
-    clearBoard();
-    gameController.clearPlayers();
-    gameController.gameOver();
-  }
-
   function clearBoard() {
     for (let i = 0; i < boardBox.length; i++) {
       boardBox[i].textContent = '';
     }
   }
 
-  function currentPlayerText(currentPlayer) {
-    displayTextForPlayer.style.display = 'inline';
-    displayCurrentPlayer.textContent = currentPlayer;
+  return {
+    currentPlayerText,
+    hideCurrentPlayer,
+    addBackground,
+    removeBackground,
+    clearBoard,
+  }
+})();
+
+const MenuController = (function() {
+  const board = Board; /////////////////////////
+  const game = GameController;
+  const render = RenderController;
+
+  const displayMenuDiv = document.querySelector('.pop-up-menu');
+
+  const firstPlayerName = document.getElementById('first-player');
+  const secondPlayerName = document.getElementById('second-player');
+
+  const startGameBtn = document.querySelector('.btn-start');
+  startGameBtn.addEventListener('click', startGame);
+
+  const getFirstPlayer = () => firstPlayerName;
+
+  function startGame() { 
+    game.startGame(firstPlayerName.value, secondPlayerName.value);
+    hideMenu();
+    game.restartGame();
+    render.currentPlayerText(firstPlayerName.value);
+  }
+
+  const chooseOpponent = document.querySelectorAll('.choose-opponent');
+  chooseOpponent.forEach(opponent => opponent.addEventListener('click', checkOpponent));
+
+  const playerVsPlayer = document.getElementById('player-player');
+  const playerVsEasy = document.getElementById('player-easy');
+  const playerVsHard = document.getElementById('player-hard');
+
+  function checkOpponent(e) {
+    let whoIsOpponent = e.target.id;
+    if (whoIsOpponent === 'player-player') {
+      enableSecondPlayer();
+    } else if (whoIsOpponent === 'player-easy') {
+      disableSecondPlayer('Easy');
+    } else if (whoIsOpponent === 'player-hard') {
+      disableSecondPlayer('Hard') 
+    }
+    return whoIsOpponent; // ???
+  }
+
+  function disableSecondPlayer(whichBot) {
+    secondPlayerName.disabled = true;
+    secondPlayerName.value = `${whichBot} AI`;
+  }
+
+  function enableSecondPlayer() {
+    secondPlayerName.disabled = false;
+    secondPlayerName.value = '';
+  }
+
+  function showMenu() {
+    displayMenuDiv.style.display = 'flex';
+    render.addBackground();
+  }
+
+  function hideMenu() {
+    displayMenuDiv.style.display = 'none';
+    render.removeBackground();
+  }
+
+  return {
+    getFirstPlayer,
+    showMenu,
+    hideMenu,
+  }
+})();
+
+const WinnerController = (function() {
+  const game = GameController;
+  const render = RenderController;
+  const menu = MenuController;
+
+  const displayWinnerDiv = document.querySelector('.pop-up-winner');
+
+  const displayWinnerH2 = document.querySelector('.show-winner-h1');
+  const displayWinnerP = document.querySelector('.show-winner');
+
+  const playAgainBtn = document.querySelector('.play-again');
+  playAgainBtn.addEventListener('click', playAgain);
+
+  const menuBtn = document.querySelector('.menu');
+  menuBtn.addEventListener('click', openMenu);
+
+  function displayGameOver(player) {
+    render.hideCurrentPlayer();
+    showWinner();
+
+    displayWinnerH2.textContent = `Winner is `;
+    displayWinnerP.textContent = `${player.getName()}`;
+  }
+
+  function displayGameOverWhenTie() {
+    render.hideCurrentPlayer();
+    showWinner();
+
+    displayWinnerH2.textContent = '';
+    displayWinnerP.textContent = `IT'S A TIE!`;
+  }
+
+  function showWinner() {
+    displayWinnerDiv.style.display = 'flex';
+    render.addBackground();
+  }
+
+  function hideWinner() {
+    displayWinnerDiv.style.display = 'none';
+    render.removeBackground();
+  }
+
+  function openMenu() {
+    hideWinner();
+    menu.showMenu(); 
+    game.restartGame();
+    render.clearBoard();
+  }
+
+  function playAgain() {
+    hideWinner();
+    game.restartGame();
+    render.currentPlayerText(menu.getFirstPlayer().value);
+    render.clearBoard();
+  }
+
+  return {
+    displayGameOver,
+    displayGameOverWhenTie,
+    hideWinner, /////// ?????
+    showWinner,
+  }
+})();
+
+const ScreenController = (function() {
+  const board = Board;
+  const game = GameController;
+  const menu = MenuController;
+  const render = RenderController;
+  const win = WinnerController;
+
+  menu.showMenu();
+
+  boardBox.forEach(box => box.addEventListener('click', play));
+
+  const restartGameBtn = document.querySelector('.restart');
+  restartGameBtn.addEventListener('click', restartGame);
+
+  const newGameBtn = document.querySelector('.new-game');
+  newGameBtn.addEventListener('click', openMenu);
+
+  function play(e) { 
+    if (e.target.textContent === '') {
+      e.target.textContent = game.getCurrentPlayerMarker();
+      board.setBoard(e.target.id, game.getCurrentPlayerMarker());
+      game.storePlayersMarkers();
+      console.log(game.getRound());
+
+      let check = game.checkWinner();
+      if (check !== undefined && game.getRound() <= 9) {
+        if (check === 'X') {
+          win.displayGameOver(game.getFirstPlayer());
+        } else if (check === 'O') {
+          win.displayGameOver(game.getSecondPlayer());
+        } else if (check === 'TIE') {
+          win.displayGameOverWhenTie();
+        }
+        game.gameOver();
+      } else {
+        game.nextRound();
+        game.nextPlayerMarker();
+        render.currentPlayerText(game.nextPlayerName());
+      }
+    }
+  }
+
+  function openMenu() {
+    win.hideWinner();
+    menu.showMenu(); 
+    game.restartGame();
+    render.clearBoard();
+  }
+
+  function restartGame() {
+    game.restartGame();
+    render.currentPlayerText(game.getFirstPlayer().getName());
+    render.clearBoard();
   }
 })();
